7658ff2d3fe9506768ecfd6bfcc59f6f0acdfd1e
Ahmad daneil Bin Wari <2023837334@student.uitm.edu.my>
2025-06-23 11:21:50 +0800

    Change from add app android to add app flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:lost_and_found_app/login.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(MaterialApp(
     initialRoute: '/',
     routes: {
